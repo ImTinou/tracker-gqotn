bfcc4e53d1c32b473cc0332c33fb7a30cd4b2297
ImTinou <tinoutestuwu@gmail.com>
2026-02-01 21:15:27 +0000

    Trigger Vercel redeploy for thumbnail endpoint

--- api/thumbnail.js
+++ api/thumbnail.js
@@ -1,4 +1,5 @@
 // Vercel Serverless Function to proxy Roblox Thumbnail API
+// Fixes CORS issues from GitHub Pages
 export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
